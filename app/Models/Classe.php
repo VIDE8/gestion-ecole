ab1a9b49ca9f177553817495ef694f15a9cd73b7
VIDE KOSSI <kossivide8@gmail.com>
2026-09-17 10:52:03 +0000

    Update Classe.php

--- app/Models/Classe.php
+++ app/Models/Classe.php
@@ -6,10 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Classe extends Model
 {
-    protected $fillable = ['nom_classe', 'niveau'];
+    protected $fillable = ['nom_classe', 'niveau', 'enseignant_id'];
 
     public function eleves()
     {
         return $this->hasMany(Eleve::class, 'classes_id');
+    }
+
+    public function enseignant()
+    {
+        return $this->belongsTo(User::class, 'enseignant_id');
+    }
+
+    public function presences()
+    {
+        return $this->hasMany(Presence::class);
+    }
+
+    public function comportements()
+    {
+        return $this->hasMany(Comportement::class);
     }
 }

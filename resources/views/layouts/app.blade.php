331cddbe8eb37cbde06026356cfc7ee93950bb69
VIDE KOSSI <kossivide8@gmail.com>
2026-09-17 21:20:05 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item me-3">
-                            <a class="nav-link fw-bold text-success" href="{{ route('classes.index') }}">Gestion des Classes</a>
+                            <a class="nav-link fw-bold text-success" href="{{ route('home') }}">Gestion des Classes</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link fw-bold text-primary" href="{{ route('eleves.index') }}">Registre des Élèves</a>

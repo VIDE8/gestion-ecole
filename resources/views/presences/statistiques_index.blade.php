532b25721ed7ed51f0197af3cefcd41b361108b2
VIDE KOSSI <kossivide8@gmail.com>
2026-09-21 10:00:00 +0000

    Create statistiques_index.blade.php

---
+++ resources/views/presences/statistiques_index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-4">
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <h3>Statistiques par classe</h3>
+        <a href="{{ route('home') }}" class="btn btn-outline-secondary btn-sm">&larr; Retour</a>
+    </div>
+
+    <ul class="list-group">
+        @forelse($classes as $classe)
+            <li class="list-group-item">
+                <a href="{{ route('presences.statistiques', $classe) }}">{{ $classe->nom_classe }}</a>
+            </li>
+        @empty
+            <li class="list-group-item text-muted">Aucune classe enregistrée.</li>
+        @endforelse
+    </ul>
+</div>
+@endsection
